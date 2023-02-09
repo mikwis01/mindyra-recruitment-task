1873087c4c6b1219c1864773bb0e086a29e38a8f
Mikołaj Wiśniewski <mikolajwisniewski01@gmail.com>
2023-02-09 21:00:43 +0100

    fix: empty const removed

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,6 +1,4 @@
 export const Header = () => {
-  const test = ""
-
   return (
     <div className="max-w-3xl w-5/6 mt-4 h-14 bg-neutral-800 text-white grid place-items-center">
       <h1 className="font-bold text-xl">Github users slider</h1>
